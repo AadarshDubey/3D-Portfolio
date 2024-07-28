c889a5c9aa786c0ce4f9d93e6cc40cc2854e3062
Aadarsh Dubey <addydubey321@gmail.com>
2024-07-28 23:58:45 +0530

    Added

---
+++ src/components/About.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Tilt } from 'react-tilt' 
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow card"
+      >
+        <div
+          options={{ max: 25, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[240px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 0.1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I am a full stack developer with a passion for creating beautiful and functional web applications. 
+        I have experience working with a variety of technologies and frameworks, and I am always looking to expand my skill set and learn new things. 
+        I am a creative problem solver with a keen eye for detail, and I am dedicated to delivering high-quality work that meets the needs of my clients.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap justify-center gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
